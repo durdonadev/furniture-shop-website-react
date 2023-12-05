4d09de72c9d2a9537c84d6f71af2e6abcab14d01
Durdona Ibragimova <durdona.dev@gmail.com>
2023-12-04 22:49:51 -0500

    Make feature component reusable using promps

--- src/pages/home/sections/features/Features.js
+++ src/pages/home/sections/features/Features.js
@@ -1,54 +1,34 @@
 import "./Features.css";
-import heart from "../../../../assets/heart.svg";
-import zap from "../../../../assets/zap.svg";
-import headphones from "../../../../assets/headphones.svg";
+import { features } from "./data";
+
+const Feature = (props) => {
+    return (
+        <div className="feature">
+            <div className="feature-icon ellipse-1">
+                <img className="icon" src={props.image} alt="heart shape" />
+            </div>
+            <div className="feature-text">
+                <h4 className="heading-6">{props.title}</h4>
+                <p className="text-md">{props.description}</p>
+            </div>
+        </div>
+    );
+};
 
 export const Features = () => {
     return (
         <section id="features-section">
             <div className="features-section container">
-                <div className="feature">
-                    <div className="feature-icon ellipse-1">
-                        <img className="icon" src={heart} alt="heart shape" />
-                    </div>
-                    <div className="feature-text">
-                        <h4 className="heading-6">Best quality</h4>
-                        <p className="text-md">
-                            Uncompromising excellence delivered in every product
-                            and experience
-                        </p>
-                    </div>
-                </div>
-
-                <div className="feature">
-                    <div className="feature-icon ellipse-2">
-                        <img className="icon" src={zap} alt="heart shape" />
-                    </div>
-                    <div className="feature-text">
-                        <h4 className="heading-6">Fastest delivery</h4>
-                        <p className="text-md">
-                            Swift and efficient shipping to bring your order to
-                            you promptly.
-                        </p>
-                    </div>
-                </div>
-
-                <div className="feature">
-                    <div className="feature-icon ellipse-3">
-                        <img
-                            className="icon"
-                            src={headphones}
-                            alt="heart shape"
+                {features.map((feature, idx) => {
+                    return (
+                        <Feature
+                            key={idx}
+                            title={feature.title}
+                            description={feature.description}
+                            image={feature.image}
                         />
-                    </div>
-                    <div className="feature-text">
-                        <h4 className="heading-6">Great support</h4>
-                        <p className="text-md">
-                            Exceptional customer service to assist and guide you
-                            effectively
-                        </p>
-                    </div>
-                </div>
+                    );
+                })}
             </div>
         </section>
     );
---
+++ src/pages/home/sections/features/data.js
@@ -0,0 +1,25 @@
+import "./Features.css";
+import heart from "../../../../assets/heart.svg";
+import zap from "../../../../assets/zap.svg";
+import headphones from "../../../../assets/headphones.svg";
+
+export const features = [
+    {
+        title: "Best quality",
+        description:
+            "Uncompromising excellence delivered in every product and experience.",
+        image: heart
+    },
+    {
+        title: "Fastest delivery",
+        description:
+            "Swift and efficient shipping to bring your order to you promptly.",
+        image: zap
+    },
+    {
+        title: "Great support",
+        description:
+            "Exceptional customer service to assist and guide you effectively.",
+        image: headphones
+    }
+];
