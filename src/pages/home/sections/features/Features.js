3c4dc7547cc5be70fcef4a98b7524c881fba9638
Durdona Ibragimova <durdona.dev@gmail.com>
2023-12-06 07:19:18 -0500

    Fix Feature component

--- src/pages/home/sections/features/Features.js
+++ src/pages/home/sections/features/Features.js
@@ -4,7 +4,7 @@ import { features } from "./data";
 const Feature = (props) => {
     return (
         <div className="feature">
-            <div className="feature-icon ellipse-1">
+            <div className={`feature-icon ${props.ellipseClass}`}>
                 <img className="icon" src={props.image} alt="heart shape" />
             </div>
             <div className="feature-text">
@@ -20,12 +20,20 @@ export const Features = () => {
         <section id="features-section">
             <div className="features-section container">
                 {features.map((feature, idx) => {
+                    const ellipseClass =
+                        idx === 1
+                            ? "ellipse-2"
+                            : idx === 2
+                            ? "ellipse-3"
+                            : "ellipse-1";
+
                     return (
                         <Feature
                             key={idx}
                             title={feature.title}
                             description={feature.description}
                             image={feature.image}
+                            ellipseClass={ellipseClass}
                         />
                     );
                 })}
