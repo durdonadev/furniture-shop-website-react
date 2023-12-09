2d0719fa0fa8c2ad7160b84b7f95a45da6441f67
Durdona Ibragimova <durdona.dev@gmail.com>
2023-12-09 01:51:12 -0500

    Implement media-query in Top-Navigation component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./base.css";
-import "./media-query.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
